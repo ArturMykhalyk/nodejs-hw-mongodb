08522a3b39dec03cbbfca15f7348ccbad00e683f
Artur_M <64428009+ArturMykhalyk@users.noreply.github.com>
2025-09-12 08:01:55 +0300

    fix patch return

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -49,7 +49,7 @@ export const patchContactController = async (req, res, next) => {
   res.json({
     status: 200,
     message: `Successfully patched a contact!`,
-    data: result,
+    data: result.contact,
   });
 };
 export const deleteContactController = async (req, res, next) => {
