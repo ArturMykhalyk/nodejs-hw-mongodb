3648791632a2abf678cc052b7cb8a38c7cd43f41
Artur_M <64428009+ArturMykhalyk@users.noreply.github.com>
2025-09-27 19:59:23 +0300

    ukr net test 2

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -6,7 +6,7 @@ import { SMTP } from '../constans/index.js';
 const transporter = nodemailer.createTransport({
   host: getEnvVar(SMTP.SMTP_HOST),
   port: Number(getEnvVar(SMTP.SMTP_PORT)),
-  secure: Number(getEnvVar(SMTP.SMTP_PORT)) === 465,
+  secure: false,
   auth: {
     user: getEnvVar(SMTP.SMTP_USER),
     pass: getEnvVar(SMTP.SMTP_PASSWORD),
